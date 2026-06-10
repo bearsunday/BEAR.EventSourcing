e920ff41677658761bf71d9bf0817d400db61c03
Akihito Koriyama <akihito.koriyama@gmail.com>
2026-06-10 12:06:24 +0900

    Use semantic resource context fixture metadata

--- tests/Fixture/ResourceRequestContext.php
+++ tests/Fixture/ResourceRequestContext.php
@@ -8,6 +8,12 @@ use Koriym\SemanticLogger\AbstractContext;
 
 final class ResourceRequestContext extends AbstractContext
 {
+    /** @psalm-suppress InvalidClassConstantType */
+    public const TYPE = 'resource_request';
+
+    /** @psalm-suppress InvalidClassConstantType */
+    public const SCHEMA_URL = 'https://bearsunday.github.io/schemas/semantic-logger/resource-request.json';
+
     /** @param array<string, mixed> $query */
     public function __construct(
         public string $uri,
--- tests/Fixture/ResourceResponseContext.php
+++ tests/Fixture/ResourceResponseContext.php
@@ -8,6 +8,12 @@ use Koriym\SemanticLogger\AbstractContext;
 
 final class ResourceResponseContext extends AbstractContext
 {
+    /** @psalm-suppress InvalidClassConstantType */
+    public const TYPE = 'resource_response';
+
+    /** @psalm-suppress InvalidClassConstantType */
+    public const SCHEMA_URL = 'https://bearsunday.github.io/schemas/semantic-logger/resource-response.json';
+
     public function __construct(
         public int $code,
         public mixed $body,
